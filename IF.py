c943457f23abfa142166dfe08f01db63bf7f6d48
AlanLin58 <t0916315791@gmail.com>
2021-11-01 00:24:50 +0800

    Second Commit

---
+++ IF.py
@@ -0,0 +1,22 @@
+score = int(input("請輸入分數:"))
+'''
+if score >100:
+    print("成績非常優!!\n請確認分數是否正確,ha ha ")
+elif score == 100:
+    print("very good")
+elif score >= 80:
+    print("good ")
+elif score >= 60:
+    print("低空飛行")
+else:
+    print("再接再厲")
+'''
+
+if score ==100:
+    print("優秀!")
+elif score >=90:
+    print("您很Good")
+elif score >=60:
+    print("再接再厲")
+else:
+    print("加油")
